8b47ebc88929dd1e2c5c5cb5e77a25c27271a60d
Alberto Candido <albertocandido98@gmail.com>
2022-07-06 20:17:39 -0300

    add req-05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import { Header } from '../components';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange({ target }) {
+    const { value } = target;
+    this.setState({ artistName: value });
+  }
+
   render() {
+    const { artistName } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleInputChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ artistName.length < 2 }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
