7aab34fd5e21cec8c6445356383f03d25b792486
Alberto Candido <albertocandido98@gmail.com>
2022-07-06 19:38:11 -0300

    add req-04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -25,6 +26,15 @@ export default class Header extends Component {
         { userName ? (
           <h2 data-testid="header-user-name">{userName}</h2>
         ) : <Loading />}
+        <Link to="/search" data-testid="link-to-search">
+          search
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          favorites
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          profile
+        </Link>
       </header>
     );
   }
