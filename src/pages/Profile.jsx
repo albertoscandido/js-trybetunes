b23dfbaf524c8d96a18370041a3d1ce47d1bc939
Alberto Candido <albertocandido98@gmail.com>
2022-07-07 21:08:18 -0300

    add req-13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
-import { Header } from '../components';
+import { Link } from 'react-router-dom';
+import { Header, Loading } from '../components';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({ user });
+  }
+
   render() {
+    const { user } = this.state;
+    // const {} = user ? user;
     return (
       <div data-testid="page-profile">
         <Header />
+        {
+          !user && <Loading />
+        }
+        {
+          user && (
+            <div>
+              <img src={ user.image } alt={ user.name } data-testid="profile-image" />
+              <Link to="/profile/edit"><button type="button">Editar perfil</button></Link>
+              <h2>{ user.name }</h2>
+              <h4>{ user.email }</h4>
+              <p>{ user.description }</p>
+            </div>
+          )
+        }
       </div>
     );
   }
