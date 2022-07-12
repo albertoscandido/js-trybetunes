1fbc9979f98aacc6848494a090c53670788c8091
Alberto Candido <albertocandido98@gmail.com>
2022-07-11 21:57:57 -0300

    feature: add login page styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import '../styles/login.css';
 import PropTypes from 'prop-types';
 import { Loading } from '../components';
 import {
   createUser,
 } from '../services/userAPI';
+import { ReactComponent as MusicImage } from '../svg/music.svg';
 
 export default class Login extends Component {
   constructor() {
@@ -43,21 +45,31 @@ export default class Login extends Component {
     } = this.state;
 
     return (
-      <div data-testid="page-login">
-        <input
-          type="text"
-          data-testid="login-name-input"
-          name="loginName"
-          onChange={ this.handleInputLoginChange }
-        />
-        <button
-          type="button"
-          disabled={ loginIsDisabled }
-          data-testid="login-submit-button"
-          onClick={ this.submitLogin }
-        >
-          Entrar
-        </button>
+      <div data-testid="page-login" className="page-login">
+        <div className="logo">
+          <div className="flex-r g-05">
+            <h1>trybe</h1>
+            <MusicImage className="music-image" />
+          </div>
+          <h2>tunes</h2>
+        </div>
+        <div className="form-login">
+          <input
+            type="text"
+            data-testid="login-name-input"
+            name="loginName"
+            onChange={ this.handleInputLoginChange }
+            placeholder="Enter your name"
+          />
+          <button
+            type="button"
+            disabled={ loginIsDisabled }
+            data-testid="login-submit-button"
+            onClick={ this.submitLogin }
+          >
+            Entrar
+          </button>
+        </div>
         { loading ? (
           <Loading />
         ) : null }
